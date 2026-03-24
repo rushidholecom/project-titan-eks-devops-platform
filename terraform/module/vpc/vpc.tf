6814e6d41b3d105f8fbbb84b34376f435fa3c065
rushidholecom <dholerushi.com@gmail.com>
2026-03-24 10:00:25 +0530

    update tags

--- terraform/module/vpc/vpc.tf
+++ terraform/module/vpc/vpc.tf
@@ -9,9 +9,13 @@ resource "aws_vpc" "titan_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.titan_vpc.id
   cidr_block = var.public_subnet_cidr
+  tags = {
+    Name = "${var.project}-public-subnet"
+  }
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id = aws_vpc.titan_vpc.id
   cidr_block = var.private_subnet_cidr
+  tags = "${var.project}-private-subnet"
 }
