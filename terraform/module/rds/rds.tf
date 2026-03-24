f9f9372f5f7f9449f6e17244961fd8999516ad0e
rushidholecom <dholerushi.com@gmail.com>
2026-03-24 16:41:45 +0530

    update

--- terraform/module/rds/rds.tf
+++ terraform/module/rds/rds.tf
@@ -1,12 +1,18 @@
 resource "aws_db_instance" "titan_rds" {
  allocated_storage = 10
+ storage_type = "gp2"
  db_name = "${var.db_name}"
  engine = "mariadb"
  engine_version = "11.8.5"
  instance_class = "db.t3.micro"
  username = var.username
  password = var.password
- 
+ vpc_security_group_ids = [aws_db_subnet_group.mariadb_subnet_group]
+ timeouts {
+    create = "3h"
+    delete = "3h"
+    update = "3h"
+  }
 }
 
 resource "aws_security_group" "mariadb_security_group" {
